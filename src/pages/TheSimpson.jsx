be3e84a215cfeed4416dc7a44167df445176c8a1
Emil-Spi <xiriusix@gmail.com>
2025-10-13 12:56:36 -0400

    v1.0.0

--- src/pages/TheSimpson.jsx
+++ src/pages/TheSimpson.jsx
@@ -8,6 +8,7 @@ export default function TheSimpson() {
   const [charater, setcharater] = useState(1)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [searchText, setSearchText] = useState('')
 
 
 
@@ -25,27 +26,82 @@ export default function TheSimpson() {
   }, [charater])
 
 
+  const paginationButton = (inputName) => {
+    if (inputName == "after" && charater < 20) {
+      setcharater(charater + 1)
+
+    } else if (inputName == "before" && charater > 1) {
+      setcharater(charater - 1)
+    }
+
+  }
+
+  const findCard = data.filter(card => {
+
+    if (!searchText) {
+      return true
+    }
+
+    const newSearchValue = searchText.toLowerCase()
+
+    const filterByName = card.name
+      .toLowerCase()
+      .includes(newSearchValue)
+
+    const filterByOccupation = card.occupation
+      .toLowerCase()
+      .includes(newSearchValue)
+
+
+    return filterByName || filterByOccupation
+
+
+  })
+
   return (
     <>
+      <main>
+      <div className="tittle">The Simpson</div>
+        <div className="searchContainer">
+          <label htmlFor="search" >Search</label>
+          <input
+            className="search"
+            id="search"
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </div>
 
-      <div>The Simpson</div>
-      <div className="container">
-        {!loading && data.map((ch) => (
-          <div key={ch.id} className="card">
-            <h2>{ch.name}</h2>
-            <img src={`https://cdn.thesimpsonsapi.com/200${ch.portrait_path
-              }`} alt={ch.name} />
-            <p><strong>Age: </strong> {ch.age}</p>
-            <p><strong>Gender: </strong>{ch.gender}</p>
-            <p><strong>Occupation: </strong>{ch.occupation}</p>
-            <p><strong>Birthdate: </strong>{ch.birthdate}</p>
-            <p><strong>Status: </strong>{ch.status}</p>
+        <div className="container">
+          {!loading && findCard.map((ch) => (
+            <div key={ch.id} className="card">
+              <h2>{ch.name}</h2>
+              <img src={`https://cdn.thesimpsonsapi.com/200${ch.portrait_path
+                }`} alt={ch.name} />
+              <p><strong>Age: </strong> {ch.age}</p>
+              <p><strong>Gender: </strong>{ch.gender}</p>
+              <p><strong>Occupation: </strong>{ch.occupation}</p>
+              <p><strong>Birthdate: </strong>{ch.birthdate}</p>
+              <p><strong>Status: </strong>{ch.status}</p>
 
-          </div>
-        ))
-        }
+            </div>
+          ))
+          }
+        </div>
 
-      </div>
+        <div className="pagination">
+          <button
+            name="before"
+            onClick={() => paginationButton('before')}
+          >←</button>
+          <p>{charater}</p>
+          <button
+            name="after"
+            onClick={() => paginationButton('after')}
+          >→</button>
+        </div>
+      </main>
     </>
   )
 }
